d9e528faac5f1ec19e2c512cd43585ced90082a6
eric-epsilla <eric@epsilla.com>
2023-08-03 13:45:10 +0800

    update

--- engine/clients/epsilla/config.py
+++ engine/clients/epsilla/config.py
@@ -3,6 +3,7 @@ from engine.base_client.distances import Distance
 EPSILLA_API_KEY = ""
 EPSILLA_ENVIRONMENT = "us-east-1-aws"
 EPSILLA_INDEX_NAME = "benchmark"
+EPSILLA_DATABASE_NAME = "benchmark"
 
 DISTANCE_MAPPING = {
     Distance.L2: "euclidean",
